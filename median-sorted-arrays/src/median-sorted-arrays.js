bd7b3fb06892305d6cfeb5a4f67a2e6aff82fce4
Talha <talhaanwar.anwar@gmail.com>
2021-02-14 23:59:09 +0530

    findMedianSortedArrays using merge step instead of using sort()

--- median-sorted-arrays/src/median-sorted-arrays.js
+++ median-sorted-arrays/src/median-sorted-arrays.js
@@ -1,15 +1,38 @@
-const findMedianSortedArrays = (nums1, nums2) => {
-  nums1.push(...nums2);
-  nums1.sort((a, b) => a - b);
-  let median;
-  if (nums1.length%2==0) {
-    // even
-    const midpoint = Math.floor(nums1.length/2) - 1;
-    median = (nums1[midpoint] + nums1[midpoint + 1])/2;
-  } else {
-    const midpoint = Math.floor(nums1.length/2);
-    median = nums1[midpoint];
+const findMedianSortedArrays = (nums1 = [], nums2 = []) => {
+  let i1 = 0;
+  let i2 = 0;
+  const len1 = nums1.length;
+  const len2 = nums2.length;
+  const len = len1 + len2;
+  if (len == 0) {
+    return null;
   }
+
+  const merged = [];
+
+  while (i1<len1 && i2<len2) {
+    if (nums1[i1]<=nums2[i2]) {
+      merged.push(nums1[i1]);
+      i1++;
+    } else {
+      merged.push(nums2[i2]);
+      i2++;
+    }
+  }
+
+  while (i1<len1) {
+    merged.push(nums1[i1]);
+    i1++;
+  }
+
+  while (i2<len2) {
+    merged.push(nums2[i2]);
+    i2++;
+  }
+
+  const isOdd = len % 2 != 0;
+  const median = isOdd ? merged[(len -1)/2] :
+  (merged[len/2] + merged[len/2 -1])/2;
   return median;
 };
 
